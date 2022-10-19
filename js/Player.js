a7251c1a2b6a5a4bdfad8f4c66f480d4a9d81ee6
deboravpereira <100171504+deboravpereira@users.noreply.github.com>
2022-10-19 14:10:30 -0300

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,16 @@
+class Player {
+  constructor (){}
+
+
+getCount(){
+  // leia os dados e armazene o valor de playerCount
+    var playerCountRef = database.ref();
+    playerCountRef.on ();
+}
+
+updateCount (count){
+  // atualize o playerCount
+  database.ref().update();
+  } 
+
+}
